e49394e37ae5662ea618b542ecfeffa904bcdeae
mtommtom <makeotom@yahoo.com>
2026-01-28 16:12:54 -0800

    made some of data2clean.py nicer looking and added distributions for the gpu dataset

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from bs4 import BeautifulSoup as bs
+import matplotlib.pyplot as plt
+import re
+import numpy as np
 
 def save_gpu(source_file, new_file, QUERY='div.center tbody tr', FIELD_QUERY='td', column_names=['video_card_name', 'pasmark_gd3_mark', 'rank', 'videocard_value', 'price_usd']):
     '''
@@ -41,11 +44,65 @@ def save_gpu(source_file, new_file, QUERY='div.center tbody tr', FIELD_QUERY='td
     df.to_csv(new_file)
 
 
+def plot_distributions(df, bin_number = 50):
+    '''
+    plot the distributions of all the columns
+
+    :param df: dataframe of housing price per block (given longitude and latitude) features to be plotted with a histogram
+    '''
+    for column_name in df.columns:
+        if pd.api.types.is_numeric_dtype(df[column_name].dtype) and column_name != "rank" and column_name != "Unnamed: 0":
+            df[df[column_name].notna()][column_name].hist(bins=bin_number)
+            plt.ylabel(f"count")
+            plt.xlabel(f"{column_name}")
+            plt.title(f"{column_name} distribution (bin size = 50)")
+            plt.savefig(f"plot/distributions/{column_name}.png")
+            plt.clf() # clear the plot
+
+
+def preprocesss(csv = 'gpu.csv'):
+    '''
+    return a clead version of the data set by making numeric numerics and ect
+    '''
+    def make_num(element):
+        '''
+        if not null convert element to a float
+        
+        :param element: either null or a string and hs format "$888.88*"
+        '''
+        if type(element) == type(''):
+            digits = re.findall(r'\d+', element)
+            num = float(digits[0]) + float(digits[1]) * .01
+            return num
+        
+        return element
+
+    df = pd.read_csv(csv)
+    df['price_usd'] = df['price_usd'].apply(make_num)
+
+    return df
+
+
+def plot_background(df):
+    '''
+    plot basic data including possible correlations and general distributions
+    
+    :param df: Description
+    '''
+    plot_distributions(df)
+    print(df[df['price_usd'].notna()])
+    fig, ax = plt.subplots()
+
+
+
 def main():
     '''
     main method to run operations
     '''
-    save_gpu(source_file='gpu.html', new_file='gpu.csv')
+    df = preprocesss()
+    plot_background(df)
+    # save_gpu(source_file='gpu.html', new_file='gpu.csv')
+
 
 
 if __name__ == '__main__':
--- dataset2Cleaning.py
+++ dataset2Cleaning.py
@@ -1,18 +1,25 @@
 import pandas as pd
 
-df = pd.read_json("dataset2.json", orient = 'index')
-df = df.reset_index()
-df = df.rename(columns={"index": "model"})
-df['Full_Price'] = df['Release Price (USD)']
-df['Full_Price'] = df['Full_Price'].fillna(df['MSRP (USD)'])
-df['Full_Price'] = df['Full_Price'].astype(str)
-df['Full_Price'] = df['Full_Price'].str.replace('$', '', regex=False)
-df['Full_Price'] = pd.to_numeric(df['Full_Price'], errors='coerce')
-df['Date'] = pd.to_datetime(df['Launch'], errors='coerce')
-df['Year'] = df['Date'].dt.year
-df_clean = df.dropna(subset=['Full_Price', 'Year'])
-df_clean['Year'] = df_clean['Year'].astype(int)
-df_clean = df_clean.sort_values(by='Year')
-cols_to_keep = ['model', 'Year', 'Full_Price']
-df_clean.to_csv('dataset2_cleaned.csv', index=False)
-df_clean.to_excel('dataset2_cleaned.xlsx', index=False)
+def main():
+    '''
+    clean data set 2 save as dataset2.json
+    '''
+    df = pd.read_json("dataset2.json", orient = 'index')
+    df = df.reset_index()
+    df = df.rename(columns={"index": "model"})
+    df['Full_Price'] = df['Release Price (USD)']
+    df['Full_Price'] = df['Full_Price'].fillna(df['MSRP (USD)'])
+    df['Full_Price'] = df['Full_Price'].astype(str)
+    df['Full_Price'] = df['Full_Price'].str.replace('$', '', regex=False)
+    df['Full_Price'] = pd.to_numeric(df['Full_Price'], errors='coerce')
+    df['Date'] = pd.to_datetime(df['Launch'], errors='coerce')
+    df['Year'] = df['Date'].dt.year
+    df_clean = df.dropna(subset=['Full_Price', 'Year'])
+    df_clean['Year'] = df_clean['Year'].astype(int)
+    df_clean = df_clean.sort_values(by='Year')
+    cols_to_keep = ['model', 'Year', 'Full_Price']
+    df_clean.to_csv('dataset2_cleaned.csv', index=False)
+    # df_clean.to_excel('dataset2_cleaned.xlsx', index=False)
+
+if __name__ == '__main__':
+    main()
